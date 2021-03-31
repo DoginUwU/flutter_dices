b48223e922eb8e141193dfdf112e7a9a395be96f
lucasfv1 <lucasfagundesvaz@gmail.com>
2021-03-31 15:41:37 -0300

    update

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal.shade700,
+        appBar: AppBar(
+          title: Text('Dadoos'),
+          backgroundColor: Colors.teal.shade900,
+        ),
+        body: Dadoos(),
+      ),
+    ),
+  );
+}
+
+class Dadoos extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
